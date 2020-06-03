92a27bed1d83d3de34b47f3619de018b623cd046
salabson4@yahoo.co.uk <salabson4@yahoo.co.uk>
2020-06-03 13:54:19 +0100

    Implement update function for radar

--- src/kalman_filter.cpp
+++ src/kalman_filter.cpp
@@ -1,4 +1,5 @@
 #include "kalman_filter.h"
+#include "cmath"
 
 using Eigen::MatrixXd;
 using Eigen::VectorXd;
@@ -52,4 +53,26 @@ void KalmanFilter::UpdateEKF(const VectorXd &z) {
   /**
    * TODO: update the state by using Extended Kalman Filter equations
    */
+    // create jacobian matrix for linearizing radar measurement
+    MatrixXd Hj(4,4);
+    float px = x_[0];
+    float py = x_[1];
+    float vx = x_[2];
+    float vy = x_[3];
+    Hj << px/sqrt(pow(px,2)+pow(py,2)), py/sqrt(pow(px,2)+pow(py,2)), 0, 0,
+        -(py/(pow(px,2)+pow(py,2))),  px/(pow(px,2)+pow(py,2)),0,0,
+        py*(vx*py-vy*px)/(pow(px,2)+pow(py,2),1.5), px*(vy*px-vx*py)/(pow(px,2)+pow(py,2),1.5), px/sqrt(pow(px,2)+pow(py,2)), py/sqrt(pow(px,2)+pow(py,2));
+
+  // Calculate measurement residual or error
+   VectorXd y = z - (Hj*x_);
+   // Calculate Kalman filter denominator
+   MatrixXd s = H_*P_*H_.transpose();
+   // Calculate Kalman gain
+   MatrixXd K = P_*H_.transpose()*s.inverse();
+  
+  // Calculate posterior or new state
+  MatrixXd I = MatrixXd::Identity(2, 2);
+  x_= x_ + K*y;
+  P_ = (I-K*H_)*P_;
+
 }
